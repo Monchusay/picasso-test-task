25d03f63a77ec8a9f00d791b1f2cee3d58505689
Monchusay <armydolgov@mail.ru>
2024-02-14 17:59:10 +0300

    Deploy fix attempt

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createHashRouter, RouterProvider} from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import PageMain from "./pages/pageMain/pageMain";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import PageContainer from "./widgets/pageContainer/pageContainer";
 import PagePost from "./pages/pagePost/pagePost";
 
-const router = createHashRouter(
-  [
-    {
-      element: <PageContainer />,
-      children: [
-        {
-          path: "/picasso-test-task",
-          element: <PageMain />,
-        },
-        {
-          path: "/picasso-test-task/post/:postId",
-          element: <PagePost />,
-        },
-      ],
-    },
-  ],
-);
+const router = createHashRouter([
+  {
+    element: <PageContainer />,
+    children: [
+      {
+        path: "/*",
+        element: <PageMain />,
+      },
+      {
+        path: "post/:postId",
+        element: <PagePost />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider store={store}>
